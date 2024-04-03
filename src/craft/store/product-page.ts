101707c68faaccb46fe0537ef6c50e7521fe6153
dropship-dev <tiena2cva@gmail.com>
2024-04-03 10:04:47 +0000

    Update from https://github.com/dropship-dev/site-seller/commit/a45d9a2e433762a537edd270034560c08a5ebca5

--- src/craft/store/product-page.ts
+++ src/craft/store/product-page.ts
@@ -1,42 +1,42 @@
-import {
-  Campaign,
-  Discount,
-  PlatformVariant,
-  Product,
-  ProductVariant,
-  Review,
-  Tag,
-  VariantCombo,
-  VariantOptions
-} from '@dropship-dev/client-api/api/store'
+// import {
+//   Campaign,
+//   Discount,
+//   PlatformVariant,
+//   Product,
+//   ProductVariant,
+//   Review,
+//   Tag,
+//   VariantCombo,
+//   VariantOptions
+// } from '@dropship-dev/client-api/api/store'
 import { create } from 'zustand'
 
-type ProductData = Product & {
-  Campaign: Campaign & {
-    listDiscount: Array<Discount>
-  }
-  VariantCombo: Array<VariantCombo>
-  ProductVariant: Array<
-    ProductVariant & {
-      PlatformVariant: PlatformVariant
-    }
-  >
-  Review: Array<Review>
-  GoogleAnalytic: Array<{
-    measurementId: string
-  }>
-  TiktokPixel: Array<{
-    pixelId: string
-  }>
-  FbPixel: Array<{
-    pixelId: string
-  }>
-  Tag: Array<Tag>
-  PlatformProduct: {
-    variantOption: VariantOptions
-    id: number
-  }
-}
+// type ProductData = Product & {
+//   Campaign: Campaign & {
+//     listDiscount: Array<Discount>
+//   }
+//   VariantCombo: Array<VariantCombo>
+//   ProductVariant: Array<
+//     ProductVariant & {
+//       PlatformVariant: PlatformVariant
+//     }
+//   >
+//   Review: Array<Review>
+//   GoogleAnalytic: Array<{
+//     measurementId: string
+//   }>
+//   TiktokPixel: Array<{
+//     pixelId: string
+//   }>
+//   FbPixel: Array<{
+//     pixelId: string
+//   }>
+//   Tag: Array<Tag>
+//   PlatformProduct: {
+//     variantOption: VariantOptions
+//     id: number
+//   }
+// }
 
 interface ProductState {
   textPlaceholders: {
