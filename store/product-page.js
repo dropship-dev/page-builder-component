a8ffd771c248e3f44f1af34df9fd81699a788346
nvti <tiena2cva@gmail.com>
2024-04-03 10:05:20 +0000

    Update build to output generated at 101707c

--- store/product-page.js
+++ store/product-page.js
@@ -1,3 +1,14 @@
+// import {
+//   Campaign,
+//   Discount,
+//   PlatformVariant,
+//   Product,
+//   ProductVariant,
+//   Review,
+//   Tag,
+//   VariantCombo,
+//   VariantOptions
+// } from '@dropship-dev/client-api/api/store'
 import { create } from 'zustand';
 export const useProductStore = create((set) => ({
     textPlaceholders: {}
